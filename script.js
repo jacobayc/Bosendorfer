de3a8a03901eececae0b3c2ca977ea8e35280f0a
jacobayc <yaechan123@naver.com>
2024-04-17 00:11:12 +0900

    [fix] 건반을 연타하는 경우 audio 지연 이슈 수정

--- script.js
+++ script.js
@@ -6,18 +6,31 @@ const keyCheckbox = document.querySelector(".keys-checkbox input")
 let audio = new Audio("tunes/a.mp3")
 let allKeys = []
 
+let isPlaying = false;
+let playQueue = []; // 재생 대기열
+
 const playTune = (key) => {
-    console.log(key)
-    audio.src = `tunes/${key}.mp3`;
-    audio.play();
-    console.log(allKeys)
+    // console.log(key)
+    if(!isPlaying) {
+        isPlaying = true;
+        audio.src = `tunes/${key}.mp3`;
+        audio.play();
+        console.log(allKeys)
 
-    const clickedKey = document.querySelector(`[data-key="${key}"]`);
-    clickedKey.classList.add('active');
-
-    setTimeout(()=> {
-      clickedKey.classList.remove('active');
-    },150)
+        const clickedKey = document.querySelector(`[data-key="${key}"]`);
+        clickedKey.classList.add('active');
+    
+        setTimeout(()=> {
+          clickedKey.classList.remove('active');
+          isPlaying = false;
+          if(playQueue.length > 0) {
+            const nextKey = playQueue.shift();
+            playTune(nextKey)
+          }
+        },150)
+    } else {
+        playQueue.push(key);
+    }
 }
 
 pianoKeys.forEach(key => {
